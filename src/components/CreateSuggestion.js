eeecaa19375f2bb0da5cf32c4fa1b03986506811
Chris Maloney <cmaloney@euronetworldwide.com>
2018-11-27 23:41:26 -0600

    finished suggestions

--- src/components/CreateSuggestion.js
+++ src/components/CreateSuggestion.js
@@ -4,7 +4,67 @@ const CreateSuggestion = {
   name: 'create-suggestion',
 
   data: function () {
-    return {};
+    return {
+      sender: null,
+      suggestion: null
+    };
+  },
+
+  methods: {
+    checkForm: function (e) {
+
+      this.errors = [];
+
+      if (!this.suggestion) {
+        this.errors.push('Suggestion required.');
+      }
+
+      e.preventDefault();
+    },
+
+    addSuggestion: function() {
+      this.$store.dispatch('setMessage', {
+        message:("Submitting your suggestion..."),
+        messageClass: 'alert-info'
+      });
+      //console.log(`t.s.si=${this.$store.sessionId}`);
+      axios.post("/suggestions", {
+          sessionId: this.$store.getters.sessionId,
+          sender: this.sender,
+          suggestion: this.suggestion,
+          timeout: 10000
+        })
+        .then((response) => {
+          if (response.status === 200 && response.data.status === "Okay") {
+            this.$store.dispatch('setMessage', {
+              message: response.data.message,
+              messageClass: 'alert-success fade show',
+              // timeout: 2000,
+              dismissible: true
+            });
+            if (response.data.sessionId) {
+              //console.log(`r.d.si=${response.data.sessionId} t.s.si=${this.$store.getters.sessionId}`);
+              this.$store.commit('setSessionId', response.data.sessionId);
+              //console.log(`t.s.si=${this.$store.getters.sessionId}`);
+            }
+          } else {
+            this.$store.dispatch('setMessage', {
+              message: response.data.message,
+              messageClass: 'alert-danger fade show',
+              // timeout: 2000,
+              dismissible: true
+            });
+          }
+        })
+        .catch((error) => {
+          this.$store.dispatch('setMessage', {
+            message: error.toString(),
+            messageClass: 'alert-danger fade show',
+            // timeout: 2000,
+            dismissible: true
+          });
+        });
+    }
   },
 
   template: `
@@ -24,16 +84,17 @@ const CreateSuggestion = {
 
           <div class="form-group">
             <label for="name">Your Name (optional):</label>
-            <input type="text" class="form-control" id="name" aria-describedby="Your Name - optional">
+            <input v-model="sender" type="text" class="form-control" id="name" aria-describedby="Your Name (optional)">
           </div>
 
           <div class="form-group">
             <label for="message">Suggestion:</label>
-            <textarea class="form-control" rows="5" id="suggestion" aria-describedby="Suggestion">
+            <textarea v-model="suggestion" class="form-control" rows="5" id="suggestion" aria-describedby="Suggestion">
             </textarea>
           </div>
 
-          <button type="submit" class="btn btn-primary mx-auto">Send</button>
+          <button class="btn btn-primary mx-auto"
+            v-on:click="addSuggestion">Send Suggestion</button>
 
         </form>
       </div>
