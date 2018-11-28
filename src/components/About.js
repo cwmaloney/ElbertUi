7706589b59de268b15cef94e18b30064eaf64388
Chris Maloney <cmaloney@euronetworldwide.com>
2018-11-28 12:51:22 -0600

    updated about...

--- src/components/About.js
+++ src/components/About.js
@@ -22,8 +22,13 @@ const About = {
               Holiday Lights on Farmstead Lane
             </h2>
             <p>
-              Holiday Lights is the creation of Mark Callegari.
+              Holiday Lights is the creation of <b>Mark Callegari</b>.
             </p>
+            <p>
+              Deanna Rose Children's Farmstead became the home of the show in 2013.
+              Over 100,000 people visit the show each year.
+            </p>
+
             <p>
               The Holiday Lights team includes:
             </p>
@@ -32,13 +37,15 @@ const About = {
               <li>Chris & Rachel Maloney</li>
               <li>Jolt Lighting</li>
               <li>Blake Stewart</li>
-              <li>Brian Jackson and Steve Bullard</li>
+              <li>Brian Jackson & Steve Bullard</li>
               <li>Mike & Jody McCamon</li>
               <li>Ken Vrana</li>
               <li>Elliot Maloney</li>
-              <li>Virgil Miles, Laura Blatter, Kathi Limbocker, Jerry Nissen, Orrin</li>
+              <li>Deanna Rose Farmsteaders: Virgil, Laura, Kathi, Jerry, Orrin, Janet, Sarah , Amanda , Brett, and Cindy</li>
             </ul>
           </div>
+
+          <p>We thank the city of Overland Park for hosting the show at the Farmstead.</li>
 
         </form>
       </div>
