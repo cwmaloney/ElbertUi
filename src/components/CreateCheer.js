380004993034dc71fa177f9d3f2c276191871cef
Chris Maloney <cmaloney@euronetworldwide.com>
2018-11-27 22:40:48 -0600

    better UI; fixed team/colors selector

--- src/components/CreateCheer.js
+++ src/components/CreateCheer.js
@@ -5,8 +5,9 @@ const CreateCheer = {
 
   data: function () {
     return {
+      cheerType: "team",
       sender: null,
-      teamName: null,
+      teamName: "Chiefs",
       colorNames: null
     };
   },
@@ -21,13 +22,17 @@ const CreateCheer = {
         messageClass: 'alert-info'
       });
       //console.log(`t.s.si=${this.$store.sessionId}`);
-      axios.post("/cheers", {
-          sessionId: this.$store.getters.sessionId,
-          sender: this.sender,
-          teamName: this.teamName,
-          teamColors: this.teamColors,
-          timeout: 10000
-        })
+      let request = {
+        sessionId: this.$store.getters.sessionId,
+        sender: this.sender,
+        timeout: 10000
+      };
+      if (this.cheerType === "team") {
+        request.teamName = this.teamName;
+      } else {
+        request.colorNames = this.colorNames;
+      }
+      axios.post("/cheers", request)
         .then((response) => {
           if (response.status === 200 && response.data.status === "Okay") {
             this.$store.dispatch('setMessage', {
@@ -70,153 +75,148 @@ const CreateCheer = {
       </nav>
       -->
 
-      <h1>COMING SOON!</H1>
-
-      <p>
-        Grizilla can help you cheer for your favorite team!
-      </p>
-
       <div class="hl-form">
+        <p>
+          Use Grizilla to cheer for your team!
+        </p>
+
         <form>
 
           <div class="form-group">
             <label for="name">Your first name (optional):</label>
             <input v-model="sender" type="text" class="form-control" id="name" aria-describedby="Your Name">
             <small class="form-text text-muted">
-              You can display your name on Grizilla with you cheer.
+              We will display your name with you cheer.
             </small>
           </div>
 
           <div class="form-group">
+            <label>Cheer by Team:</label>
+            <div class="form-check form-check-inline">
+              <input class="form-check-input" type="radio" name="cheerBy" id="cheerForTeam" value="team" v-model="cheerType">
+              <label class="form-check-label" for="cheerForTeam">Name</label>
+            </div>
+
+            <div class="form-check form-check-inline">
+              <input class="form-check-input" type="radio" name="cheerBy" id="cheerForColors" value="colors" v-model="cheerType">
+              <label class="form-check-label" for="cheerForColors">Colors</label>
+            </div>
+            <!--
             <small class="form-text text-muted">
-              Choose a team name or team colors:
+              If your team is not listed, choose your colors.
             </small>
-            <ul class="nav nav-tabs" id="cheerTabs" role="tablist">
-              <li class="nav-item">
-                <a class="nav-link active" id="name-tab" data-toggle="tab" href="#nameTabContent" role="tab">Team Name</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" id="colors-tab" data-toggle="tab" href="#colorsTabContent" role="tab">Team Colors</a>
-              </li>
-            </ul>
+            -->
+          </div>
 
-            <form class="hl-form">
-              <div class="tab-content" id="cheerTabContent">
 
-                <div class="tab-pane fade show active" id="nameTabContent" role="tabpanel">
-                  <div class="form-group">
-                    <label for="teamName">Choose a team:</label>
-                    <select v-model="teamName" class="form-control" id="teamName">
-                      <option>Santa</option>
-                      <option>Rudolph</option>
-                      <option>Raindeer</option>
-                      <option>Grinch</option>
-                      <option>Snow</option>
-                      <option>Halloween</option>
-                      <option>Rainbow</option>,
+          <div class="form-group">
+            <label for="teamName" class="col-form-label col-form-label-sm">Choose a team:</label>
+              <select v-model="teamName" class="form-control" id="teamName" :disabled="cheerType === 'colors'">
+                <option>Santa</option>
+                <option>Rudolph</option>
+                <option>Raindeer</option>
+                <option>Grinch</option>
+                <option>Snow</option>
+                <option>Halloween</option>
+                <option>Rainbow</option>,
+                <option>USA</option>
 
-                      <option>Chiefs</option>
-                      <option>Royals</option>
-                      <option>Sporting KC</option>
+                <option>Chiefs</option>
+                <option>Royals</option>
+                <option>Sporting KC</option>
 
-                      <option>Baylor</option>
-                      <option>Iowa</option>
-                      <option>Iowa State</option>
-                      <option>Kansas</option>
-                      <option>Kansas State</option>
-                      <option>Mavericks</option>
-                      <option>MNU</option>
-                      <option>Missouri</option>
-                      <option>Nebraska</option>
-                      <option>Neptunes</option>
-                      <option>Oklahoma</option>
-                      <option>Oklahoma State</option>
-                      <option>Pittsburg State</option>
-                      <option>Rockhurst</option>
-                      <option>Roos</option>
-                      <option>Texas Christian</option>
-                      <option>Texas</option>
-                      <option>Texas Tech</option>
-                      <option>USA</option>
-                    </select>
-                  </div>
-                </div>
+                <option>Baylor</option>
+                <option>Iowa</option>
+                <option>Iowa State</option>
+                <option>Kansas</option>
+                <option>Kansas State</option>
+                <option>Mavericks</option>
+                <option>MNU</option>
+                <option>Missouri</option>
+                <option>Nebraska</option>
+                <option>Neptunes</option>
+                <option>Oklahoma</option>
+                <option>Oklahoma State</option>
+                <option>Pittsburg State</option>
+                <option>Rockhurst</option>
+                <option>UMKC</option>
+                <option>Texas Christian</option>
+                <option>Texas</option>
+                <option>Texas Tech</option>
+              </select>
+            </div>
 
-                <div class="tab-pane fade" id="colorsTabContent" role="tabpanel">
-                  <div class="form-group">
-                    <label for="colors">Choose one or more colors:</label>
-                    <select v-model="colorNames" multiple class="form-control" id="colors">
-                      <option>White</option>
-                      <option>Snow</option>
-                      <option>Celadon</option>
-                      <option>Gray</option>
-                      <option>Silver</option>
+            <div class="form-group">
+              <label for="colors" class="col-form-label col-form-label-sm">Choose one or more colors:</label>
+              <select v-model="colorNames" multiple class="form-control" id="colors"
+                style="height: 120px" :disabled="cheerType === 'team'">
+                <option>White</option>
+                <option>Snow</option>
+                <option>Celadon</option>
+                <option>Gray</option>
+                <option>Silver</option>
 
-                      <option>Red</option>
-                      <option>Crimson</option>
-                      <option>Dark Red</option>
-                      <option>Scarlet</option>
+                <option>Red</option>
+                <option>Crimson</option>
+                <option>Dark Red</option>
+                <option>Scarlet</option>
 
-                      <option>Pink</option>
-                      <option>Dark Pink</option>
-                      <option>Maroon</option>
-                      <option>Fuchsia</option>
-                      <option>Magenta</option>
+                <option>Pink</option>
+                <option>Dark Pink</option>
+                <option>Maroon</option>
+                <option>Fuchsia</option>
+                <option>Magenta</option>
 
-                      <option>Orange</option>
-                      <option>Orange Red</option>
+                <option>Orange</option>
+                <option>Orange Red</option>
 
-                      <option>Yellow</option>
+                <option>Yellow</option>
 
-                      <option>Cream</option>
-                      <option>brown</option>
-                      <option>Dark Brown</option>
-                      <option>Gold</option>
+                <option>Cream</option>
+                <option>Brown</option>
+                <option>Dark Brown</option>
+                <option>Gold</option>
 
-                      <option>Yellow Green</option>
-                      <option>Chartreuse</option>
+                <option>Yellow Green</option>
+                <option>Chartreuse</option>
 
-                      <option>Green</option>
-                      <option>Dark Green</option>
-                      <option>Grinch Green</option>
-                      <option>Olive</option>
-                      <option>Turquoise</option>
-                      <option>Dark Turquoise</option>
-                      <option>Lime</option>
-                      <option>Teal</option>
+                <option>Green</option>
+                <option>Dark Green</option>
+                <option>Grinch Green</option>
+                <option>Olive</option>
+                <option>Turquoise</option>
+                <option>Dark Turquoise</option>
+                <option>Lime</option>
+                <option>Teal</option>
 
-                      <option>BlueGreen</option>
-                      <option>Cyan</option>
-                      <option>Dark Cyan</option>
+                <option>BlueGreen</option>
+                <option>Cyan</option>
+                <option>Dark Cyan</option>
 
-                      <option>Blue</option>
-                      <option>Light Blue</option>
-                      <option>Corn Flower Blue</option>
-                      <option>Dark Blue</option>
-                      <option>Royal Blue</option>
-                      <option>Navy</option>
-                      <option>MidnightBlue</option>
-                      <option>Sporting Blue</option>
+                <option>Blue</option>
+                <option>Light Blue</option>
+                <option>Corn Flower Blue</option>
+                <option>Dark Blue</option>
+                <option>Royal Blue</option>
+                <option>Navy</option>
+                <option>Midnigh tBlue</option>
+                <option>Sporting Blue</option>
 
-                      <option>Indigo</option>
-                      <option>Dark Indigo</option>
+                <option>Indigo</option>
+                <option>Dark Indigo</option>
 
-                      <option>BlueViolet</option>
+                <option>BlueViolet</option>
 
-                      <option>Purple</option>
-                      <option>Royal Purple</option>
-                      <option>Horned Frog Purple</option>
+                <option>Purple</option>
+                <option>Royal Purple</option>
+                <option>Horned Frog Purple</option>
 
-                      <option>Violet</option>
-                      <option>Dark Violet</option>
+                <option>Violet</option>
+                <option>Dark Violet</option>
 
-                      <option>Black</option>
-                    </select>
-                  </div>
-                </div>
-              </div>
-            </form>
-
+                <option>Black</option>
+              </select>
+            </div>
           </div>
 
           <button class="btn btn-primary mx-auto"
