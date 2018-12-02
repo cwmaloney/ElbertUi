6227a177f2c8542b8bb1066ec650472d18a81f53
Chris Maloney <cmaloney@euronetworldwide.com>
2018-12-02 09:59:50 -0600

    WIP backup

--- src/components/Snakes.js
+++ src/components/Snakes.js
@@ -8,6 +8,7 @@ const Snakes = {
       sender: "",
       registered: false,
       gameId: null,
+      snakeColor: null,
       gameActive: false
     };
   },
@@ -17,62 +18,87 @@ const Snakes = {
       e.preventDefault();
     },
 
-    onRegistereResponse(response) {
-      if (response.status === 200) {
-        if ( esponse.data.status === "Okay") {
-          this.$store.dispatch('setMessage', {
-            message: response.data.message,
-            messageClass: 'alert-success fade show',
-            // timeout: 2000,
-            dismissible: true
-          });
-          if (response.data.sessionId) {
-            //console.log(`r.d.si=${response.data.sessionId} t.s.si=${this.$store.getters.sessionId}`);
-            this.$store.commit('setSessionId', response.data.sessionId);
-            //console.log(`t.s.si=${this.$store.getters.sessionId}`);
-          }
-        } else {
-          this.$store.dispatch('setMessage', {
-            message: response.data.message,
-            messageClass: 'alert-danger fade show',
-            // timeout: 2000,
-            dismissible: true
-          });
-        }
-      } else {
-        this.$store.dispatch('setMessage', {
-          message: `Unable to contact Gridzilla. Please try again. (error=${response.status})`,
-          messageClass: 'alert-danger fade show',
-          // timeout: 2000,
-          dismissible: true
-        });
-      }
+    setMessage: function(messageObject) {
+      this.$store.dispatch("setMessage", messageObject);
     },
 
-    onErrorResponse(error, callName) {
-      const message = ((callName) ? `${callName} Failed. ` : `Unable to contact Gridzilla. `) + `Please try again. (${error.toString()})`;
-      this.$store.dispatch('setMessage', {
-        message,
-        messageClass: 'alert-danger fade show',
-        // timeout: 2000,
-        dismissible: true
+    addMessage: function(messageObject) {
+      this.$store.dispatch("addMessage", messageObject);
+    },
+
+    initializeSocket() {
+
+      // set up the connection to the game server
+      if (this.socket) {
+        return;
+      }
+
+      this.setMessage("Connectiong...");
+      this.socket = io(gridzillaBaseUrl, { reconnectionAttempts: 3 });
+
+      this.socket.on('connect', function() {
+        this.setMessage( {message: 'Connected to Gridzilla.'} );
+        console.log(`socket id= ${this.socket.id}`);
+      }.bind(this));
+
+      this.socket.on('disconnect', function() {
+        this.setMessage( {message: 'Disconnect from Gridzilla!', messageClass: 'alert-danger fade show'} );
+      }.bind(this));
+
+      this.socket.on('message', function (messageObject) {
+        this.setMessage( messageObject );
       });
+
+      this.socket.on('reconnect_failed', function() {
+        this.setMessage( {message: 'Reconnection to Gridzilla failed!', messageClass: 'alert-danger fade show'} );
+      }.bind(this));
+
+      this.socket.on('ping', function(data) {
+        const roundTripTime = Date.now() - this.pingTimestamp;
+
+        this.addMessage( { message: `The current game is ${data.activeGameId}.`, timeout: 1500 } );
+        this.addMessage( { message: `(ping round trip time is ${roundTripTime}.)`, timeout: 1500 } );
+
+        setTimeout(this.sendPing.bind(this), 2000);
+      }.bind(this));
+
+      this.socket.on('gameStarted', function(data) {
+        this.addMessage( {message: `Your game has started. You are the ${this.snakeColor} snake.`, timeout: 10000} );
+        this.gameActive = true;
+      }.bind(this));
+
+      this.socket.on('gameEnded', function(gameResults) {
+        this.setMessage(`Your game has ended.`);
+        this.gameActive = true;
+        this.gameId = null;
+      }.bind(this));
+
+      this.socket.on('state', function(data) {
+        this.displayBoard(data.board);
+      }.bind(this));
+
+      this.socket.on('message', function(data) {
+        this.addMessage(data.messageObject);
+      }.bind(this));
     },
 
     register: function() {
-      this.$store.dispatch('setMessage', {
-        message:("Registering..."),
-        messageClass: 'alert-info'
-      });
-      //console.log(`t.s.si=${this.$store.sessionId}`);
-      axios.post("/snakes.register", {
-          sessionId: this.$store.getters.sessionId,
-          sender: this.sender,
-          timeout: 10000
-        })
-        .then((response) => { this.onRegisterResponse(response); })
-        .catch((error) => { this.onErrorResponse(error, "Registration"); });
+      this.setMessage({message:"Registering...", messageClass: 'alert-info'});
+      this.socket.emit('register', { name: this.sender, snakeColor: this.snakeColor });
+    },
+
+    // send ping to check status of game and measure latancy
+    sendPing() {
+      this.socket.emit('ping', this.gameId);
+      this.pingTimestamp = Date.now();
+    },
+
+    displayBoard(board) {
     }
+
+  // createRandomPlayerName() {
+  //   return "Player " + Math.random().toString(5).substring(2);
+  // },
   },
 
   template: `
